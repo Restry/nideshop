655ae248e6fdf19203ec31f453a086a374637f90
Louis <michael.wei@foxmail.com>
2018-04-26 17:22:54 +0800

    stop create folder

--- src/common/bootstrap/master.js
+++ src/common/bootstrap/master.js
@@ -5,6 +5,6 @@ const FileUtil = require('../fileutil');
 
 FileUtil.clearTmpImg(); // 清空临时目录
 
-FileUtil.initImgDir(); // 初始化目录
+// FileUtil.initImgDir(); // 初始化目录
 
 console.log("Manster process started");
